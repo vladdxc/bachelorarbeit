dc73ca3ad83f050254f974e7e7fd27eca911af96
Vlad <vlad.andrei@tum.de>
2019-03-14 14:32:49 +0100

    Changed __data_generation(). Now layers in vae.script don't work properly.

--- autoencoder/vae_script.py
+++ autoencoder/vae_script.py
@@ -3,74 +3,46 @@ import numpy as np
 from keras.layers import Lambda, Dense, Input, Flatten
 from keras.models import Model
 from keras.losses import mse
-from import_patches import ImportPatches
+from keras import callbacks
+from importpatchesutil import ImportPatches
+import matplotlib.pyplot as plt
 import glob
+import networks as nt
+
+data_params = {'dim': (128, 128),
+               'batch_size': 1,
+               'n_classes': 2,
+               'n_channels': 2,
+               'fmt': 'mat',
+               'norm_type': 'max1',
+               'shuffle': True} #Dictionary containing the parameters for the data generator
 
 
-path = "D:\patches_S1B_IW_GRDH_20180823\*.tif"
-fls = glob.glob(path)
-len_path = len(fls)
-
-params = {'dim': (128,128,2),
-          'batch_size': 32,
-          'n_classes': 1,
-          'shuffle': True}
-partition = {'train': fls}
-
-def sampling(args):
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    epsilon = K.random_normal(shape=(batch, dim))
-
-
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
-
-def elbo_loss(args):
-
-    z_mean, z_log_var = args
-    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-
-    return K.mean(kl_loss)
-
-original_dim = 128*128*2
-input_shape = (128,128,2)
+'Model parameters'
+input_shape = (*data_params['dim'], data_params['n_channels'])
+original_dim = np.prod(input_shape)
 intermediate_dim = 512
 latent_dim = 2
+mode = 'gauss'
+args = [input_shape, original_dim, intermediate_dim, latent_dim]
 
-inputs = Input(shape=input_shape, name='encoder_input')
-flattened = Flatten()(inputs)
-x = Dense(intermediate_dim, activation='relu')(flattened)
-z_mean = Dense(latent_dim, name='z_mean')(x)
-z_log_var = Dense(latent_dim, name='z_log_var')(x)
-
-z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
-
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-
-latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-outputs = Dense(original_dim, activation='sigmoid')(x)
-decoder = Model(latent_inputs, outputs, name='decoder')
-
-outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs, name='vae_mlp')
-
-encoder.summary()
-decoder.summary()
-
-reconstruction_loss = mse(flattened, outputs)
-reconstruction_loss *= original_dim
-elbo = elbo_loss([z_mean, z_log_var])
-
-vae_loss = reconstruction_loss + elbo
-vae.add_loss(vae_loss)
-
+'Compile model and callbacks'
+encoder, decoder, vae = nt.autoencoder(args=args, mode=mode)
 vae.compile(optimizer='adam')
 
-if __name__ == '__main__':
+terminate_on_nan = callbacks.TerminateOnNaN()
+early_stop = callbacks.EarlyStopping(monitor='loss', patience=5, mode='min')
+callbacks_list = [terminate_on_nan, early_stop]
 
-    xtrain = ImportPatches(partition['train'], **params)
-    vae.fit_generator(generator=xtrain, use_multiprocessing= True, workers= 3)
+
+if __name__ == '__main__':
+    path = 'C:\\Users\\Vlad\\Desktop\\SatData\\SAR\\GRD\\Patches_mat\\*.mat'
+    fls = glob.glob(path)
+    ids = fls[:3]
+    xtrain = ImportPatches(path_IDs=ids, **data_params)
+
+    vae.fit_generator(generator=xtrain, use_multiprocessing=True, workers=6, epochs=3, callbacks=callbacks_list)
+    # vae.save('gauss_vae_mlp_s1b20180823.h5')
+    # encoder.save('enc_gauss_vae_mlp_s1b20180823.h5')
+    # decoder.save('dec_gauss_vae_mlp_s1b20180823.h5')
+    #plot_progress(vae_history)
